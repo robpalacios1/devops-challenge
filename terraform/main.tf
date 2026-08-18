bb944b672e75cfce284440aab03cb0d1e7f9d4a4
roberto <robpalacios1@hotmail.com>
2026-08-18 15:11:34 -0500

    fix error validations and and indetation with terraform fmt command

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,190 +1,182 @@
 resource "random_id" "suffix" {
-    byte_length = 4
+  byte_length = 4
 }
 
 locals {
-    name_prefix = "${var.app_name}-${var.environment}-${random_id_suffix.hex}"
+  name_prefix = "${var.app_name}-${var.environment}-${random_id.suffix.hex}"
 }
 
-data "aws_cannonical_user_id" "current" {}
+data "aws_canonical_user_id" "current" {}
 
 # Private S3 bucket tat store  the built react application
 resource "aws_s3_bucket" "site" {
-    bucket = "${local.name_prefix}-site"
+  bucket = "${local.name_prefix}-site"
 }
 
 resource "aws_s3_bucket_public_access_block" "site" {
-    bucket = aws_s3_bucket.site.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_public_access_block" "site" {
-    bucket = aws_s3_bucket.site.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.site.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "site" {
-    bucket = aws_s3_bucket.site.id
-    versioning_configuration {
-        status = "Enabled"
-    }
+  bucket = aws_s3_bucket.site.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "site" {
-    bucket = aws_s3_bucket.site.id
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
+  bucket = aws_s3_bucket.site.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 
 # S3 bucket that store cloudfront logs
 resource "aws_s3_bucket" "logs" {
-    bucket = "${local.name_prefix}-logs"
+  bucket = "${local.name_prefix}-logs"
 }
 
-resource "aws_s3_s3_bucket_ownership_controls" "logs" {
-    bucket = aws_s3_bucket.logs.id
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "logs" {
-    bucket = aws_s3_bucket.logs.id
-    depends_on = [aws_s3_s3_bucket_ownership_controls.logs]
+  bucket     = aws_s3_bucket.logs.id
+  depends_on = [aws_s3_bucket_ownership_controls.logs]
 
-    access_control_policy {
-        owner {
-            id = data.aws_cannonical_user_id.current.id
-        }
-
-        grant {
-            grantee {
-                type = "CanonicalUser"
-                id   = data.aws_cannonical_user_id.current.id
-            }
-            permission = "FULL_CONTROL"
-        }
+  access_control_policy {
+    owner {
+      id = data.aws_canonical_user_id.current.id
     }
+
+    grant {
+      grantee {
+        type = "CanonicalUser"
+        id   = data.aws_canonical_user_id.current.id
+      }
+      permission = "FULL_CONTROL"
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "logs" {
-    bucket = aws_s3_bucket.logs.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "logs" {
-    bucket = aws_s3_bucket.logs.id
-    rule {
-        id     = "Expire old logs"
-        status = "Enabled"
-        filter {}
-        expiration {
-            days = 30
-        }
+  bucket = aws_s3_bucket.logs.id
+  rule {
+    id     = "Expire old logs"
+    status = "Enabled"
+    filter {}
+    expiration {
+      days = 30
     }
+  }
 }
 
 # origin access control for cloudfront to access the private s3 bucket
 resource "aws_cloudfront_origin_access_control" "site" {
-    name = "${local.name_prefix}-site"
-    description = "Origin access control for ${local.name_prefix}-site"
-    origin_access_control_origin_type = "s3"
-    signing_behavior = "always"
-    signing_protocol = "sigv4"
+  name                              = "${local.name_prefix}-site"
+  description                       = "Origin access control for ${local.name_prefix}-site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 # cloudfront distribution to serve the react application from the private s3 bucket
 resource "aws_cloudfront_distribution" "site" {
-    enabled = true
-    is_ipv6_enabled = true
-    default_root_object = "index.html"
-    comment = "${var.app_name} - ${var.environment}"
-    price_class = "PriceClass100"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  comment             = "${var.app_name} - ${var.environment}"
+  price_class         = "PriceClass_100"
 
-    origin {
-      domain_name = aws_s3_bucket.site.bucket_regional_domain_name
-      origin_id = local.name_prefix
-      origin_access_control_id = aws_cloudfront_origin_access_control.site.id
-    }
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = local.name_prefix
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+  }
 
-    default_cache_behavior {
-      allowed_methods = [ "GET", "HEAD" ]
-      cached_methods = [ "GET", "HEAD" ]
-      target_origin_id = local.name_prefix
-      viewer_protocol_policy = "redirect-to-https"
-      compress = true
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.name_prefix
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
 
-      forwarded_values {
-        query_string = false
-        cookies {
-          forward = "none"
-        }
-      }
-
-      min_ttl = 0
-      default_ttl = 3600
-      max_ttl = 86400
-    }
-
-    custom_error_response {
-      error_code = 403
-      response_code = 200
-      response_page_path = "/index.html"
-    }
-
-    custom_error_response {
-      error_code = 404
-      response_code = 200
-      response_page_path = "/index.html"
-    }
-
-    logging_config {
-      bucket = aws_s3_bucket.logs.bucket_domain_name
-      prefix = "cloudfront/${var.environment}"
-      include_cookies = false
-    }
-
-    restrictions {
-      geo_restriction {
-        restriction_type = "none"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
       }
     }
 
-    viewer_certificate {
-      cloudfront_default_certificate = true
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  logging_config {
+    bucket          = aws_s3_bucket.logs.bucket_domain_name
+    prefix          = "cloudfront/${var.environment}"
+    include_cookies = false
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
 # Bucket Police
 data "aws_iam_policy_document" "site" {
   statement {
-    sid = "AllowCloudFrontServicePrincipalReadOnly"
-    effect = "Allow"
-    actions = [ "s3LGetObject" ]
+    sid     = "AllowCloudFrontServicePrincipalReadOnly"
+    effect  = "Allow"
+    actions = ["s3LGetObject"]
 
-    resources = [ "${aws_s3_bucket.site.arn}/*" ]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["cloudfront.amazonaws.com"]
     }
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "AWS:sourceARN"
-      values = [aws_cloudfront_distribution.site.arn]
+      values   = [aws_cloudfront_distribution.site.arn]
     }
   }
 }
